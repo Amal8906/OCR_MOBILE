5c2117365824526bdd3ab374aa541be53da5562e
Amal8906 <amal_maitra@live.com>
2020-06-29 09:11:54 +0530

    Create pages.component.ts

---
+++ src/app/pages/pages.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from '@angular/core';
+import { MenuController } from '@ionic/angular';
+
+@Component({
+  selector: 'app-pages',
+  templateUrl: './pages.component.html',
+  styleUrls: ['./pages.component.scss'],
+})
+export class PagesComponent implements OnInit {
+  appPages = [
+    {
+      title: 'Schedule',
+      url: '/app/tabs/schedule',
+      icon: 'calendar'
+    },
+    {
+      title: 'Speakers',
+      url: '/app/tabs/speakers',
+      icon: 'people'
+    },
+    {
+      title: 'Map',
+      url: '/app/tabs/map',
+      icon: 'map'
+    },
+    {
+      title: 'About',
+      url: '/app/tabs/about',
+      icon: 'information-circle'
+    }
+  ];
+  loggedIn = false;
+  dark = false;
+
+  constructor(private menu: MenuController,) { }
+
+  ngOnInit() {
+    this.menu.enable(true)
+  }
+
+}
