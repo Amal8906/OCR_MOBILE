3426e2e6415a75cc86028032627ea7cbcff82b40
Amal8906 <amal_maitra@live.com>
2020-06-29 09:06:48 +0530

    Create api-url.ts

---
+++ src/app/@core/constant/api-url.ts
@@ -0,0 +1,10 @@
+export const BASE_URL = 'http://localhost:3000/';
+// export  const BASE_URL = 'http://192.168.0.104:80/';
+//  export const BASE_URL = 'http://148.72.207.113:8097/';
+
+
+export const API_LOGIN     = BASE_URL + 'auth/login';
+
+
+
+
